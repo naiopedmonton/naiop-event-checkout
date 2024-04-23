63a5368e4574e7bfbce45675a3a77ced21ef4e9e
Scott Dohei <scottdohei@gmail.com>
2024-04-22 21:02:58 -0600

    block scaffolding

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'beda29867c82e525df63');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e54f8dfce95c43bf01fd');
