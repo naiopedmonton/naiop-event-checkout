33390a228bc33d0553e068828b580886b081b851
Scott Dohei <dohei@dohei>
2024-05-16 20:31:25 -0600

    validate course registration fields

--- naiop-event-checkout.php
+++ naiop-event-checkout.php
@@ -122,7 +122,7 @@ function course_demographic_check($value) {
 	echo '<p class="form-row check-row">';
 		echo '<span class="woocommerce-input-wrapper">';
 			echo '<label class="checkbox">';
-				echo '<input type="checkbox" name="demo" value="' . $value . '">';
+				echo '<input type="checkbox" name="naiop_demo" value="' . $value . '">';
 				echo ' ' . $value;
 			echo '</label>';
 		echo '</span>';
@@ -147,49 +147,49 @@ function course_fields() {
 		echo '<label for="broker">Are you planning to take the RECA licensing exam (to become a broker)? Please note, if you select ‘yes’ you must already have a RECA CON-ID prior to purchasing this course and will need to provide the number below. Don’t have a RECA CON-ID yet? Get yours <a href="https://public.myreca.ca/">here</a>.&nbsp;<abbr class="required" title="required">*</abbr></label>';
 		echo '<div style="clear: both; padding-left: 2rem; display: flex; gap: 20px">';
 			echo '<label class="checkbox">';
-				echo '<input type="radio" name="broker" value="Yes"> Yes';
+				echo '<input type="radio" name="naiop_broker" value="Yes"> Yes';
 			echo '</label>';
 			echo '<label class="checkbox">';
-				echo '<input type="radio" name="broker" value="No"> No';
+				echo '<input type="radio" name="naiop_broker" value="No"> No';
 			echo '</label>';
 		echo '</div>';
 	echo '</div>';
 
-	echo '<p class="form-row form-row-first validate-required" id="reg_fname">';
-		echo '<label for="reg_fname">First Name&nbsp;<abbr class="required" title="required">*</abbr></label>';
+	echo '<p class="form-row form-row-first validate-required" id="naiop_fname">';
+		echo '<label for="naiop_fname">First Name&nbsp;<abbr class="required" title="required">*</abbr></label>';
 		echo '<span class="woocommerce-input-wrapper">';
-			echo '<input type="text" class="input-text " name="reg_fname" id="reg_fname" placeholder="" value="" autocomplete="reg-fname">';
+			echo '<input type="text" class="input-text " name="naiop_fname" id="naiop_fname" placeholder="" value="" autocomplete="reg-fname">';
 		echo '</span>';
 	echo '</p>';
-	echo '<p class="form-row form-row-last validate-required" id="reg_lname">';
-		echo '<label for="reg_lname">Last Name&nbsp;<abbr class="required" title="required">*</abbr></label>';
+	echo '<p class="form-row form-row-last validate-required" id="naiop_lname">';
+		echo '<label for="naiop_lname">Last Name&nbsp;<abbr class="required" title="required">*</abbr></label>';
 		echo '<span class="woocommerce-input-wrapper">';
-			echo '<input type="text" class="input-text" name="reg_lname" id="reg_lname" placeholder="" value="" autocomplete="reg-lname">';
+			echo '<input type="text" class="input-text" name="naiop_lname" id="naiop_lname" placeholder="" value="" autocomplete="reg-lname">';
 		echo '</span>';
 	echo '</p>';
-	echo '<p class="form-row validate-required" id="reg_email">';
-		echo '<label for="reg_email">Email&nbsp;<abbr class="required" title="required">*</abbr></label>';
+	echo '<p class="form-row validate-required" id="naiop_email">';
+		echo '<label for="naiop_email">Email&nbsp;<abbr class="required" title="required">*</abbr></label>';
 		echo '<span class="woocommerce-input-wrapper">';
-			echo '<input type="text" class="input-text" name="reg_email" id="reg_email" placeholder="" value="" autocomplete="reg-email">';
+			echo '<input type="text" class="input-text" name="naiop_email" id="naiop_email" placeholder="" value="" autocomplete="reg-email">';
 		echo '</span>';
 	echo '</p>';
-	echo '<p class="form-row validate-required" id="reg_phone">';
-		echo '<label for="reg_phone">Phone&nbsp;<abbr class="required" title="required">*</abbr></label>';
+	echo '<p class="form-row validate-required" id="naiop_phone">';
+		echo '<label for="naiop_phone">Phone&nbsp;<abbr class="required" title="required">*</abbr></label>';
 		echo '<span class="woocommerce-input-wrapper">';
-			echo '<input type="text" class="input-text" name="reg_phone" id="reg_phone" placeholder="" value="" autocomplete="reg-phone">';
+			echo '<input type="text" class="input-text" name="naiop_phone" id="naiop_phone" placeholder="" value="" autocomplete="reg-phone">';
 		echo '</span>';
 	echo '</p>';
-	echo '<p class="form-row validate-required" id="reg_company">';
-		echo '<label for="reg_company">Company (optional)</label>';
+	echo '<p class="form-row validate-required" id="naiop_company">';
+		echo '<label for="naiop_company">Company (optional)</label>';
 		echo '<span class="woocommerce-input-wrapper">';
-			echo '<input type="text" class="input-text" name="reg_company" id="reg_company" placeholder="" value="" autocomplete="reg-company">';
+			echo '<input type="text" class="input-text" name="naiop_company" id="naiop_company" placeholder="" value="" autocomplete="reg-company">';
 		echo '</span>';
 	echo '</p>';
 
-	echo '<p class="form-row validate-required" id="reca_con_id">';
-		echo '<label for="reca_id">RECA CON-ID (optional)</label>';
+	echo '<p class="form-row validate-required" id="naiop_reca_id">';
+		echo '<label for="naiop_reca_id">RECA CON-ID (optional)</label>';
 		echo '<span class="woocommerce-input-wrapper">';
-			echo '<input type="text" class="input-text " name="reca_con_id" id="reca_id" placeholder="" value="" autocomplete="reca-con-id">';
+			echo '<input type="text" class="input-text " name="naiop_reca_id" id="naiop_reca_id" placeholder="" value="" autocomplete="reca-con-id">';
 		echo '</span>';
 	echo '</p>';
 }
@@ -202,6 +202,16 @@ function is_event_cart_item($cart_item) {
 function is_course_cart_item($cart_item) {
 	$course_product_ids = array(955299, 6576, 6578); //dohei-test, prod, prod
 	return ($cart_item['quantity'] > 0 && in_array($cart_item['product_id'], $course_product_ids));
+}
+
+function cart_requires_course_registration() {
+	$course_in_cart = false;
+	foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+		if (is_course_cart_item($cart_item)) {
+			return true;
+		}
+	}
+	return false;
 }
 
 add_filter('woocommerce_checkout_after_customer_details', 'naiop_checkout_end', 10);
@@ -242,7 +252,28 @@ function discount_real_estate_bundle($cart) {
 	}
 }
 
-//add_action( 'woocommerce_after_checkout_validation', 'naiop_checkout_validation', 20, 2 );
+add_action( 'woocommerce_after_checkout_validation', 'naiop_checkout_validation', 20, 2 );
 function naiop_checkout_validation($data, $errors) {
-	error_log(print_r($data, true));
+	if (cart_requires_course_registration()) {
+		if (!isset($_POST['naiop_demo']) || strlen($_POST["naiop_demo"]) <= 0) {
+			$errors->add('validation', __('Please indicate your current employment.'));
+		}
+		if (!isset($_POST['naiop_broker']) || strlen($_POST["naiop_broker"]) <= 0) {
+			$errors->add('validation', __('Please indicate whether or not you intend to take the RECA licensing exam.'));
+		}
+		if (strlen($_POST["naiop_fname"]) <= 0) {
+			$errors->add('validation', __('Please enter a first name for Course Registration.'));
+		}
+		if (strlen($_POST["naiop_lname"]) <= 0) {
+			$errors->add('validation', __('Please enter a last name for Course Registration.'));
+		}
+		if (strlen($_POST["naiop_email"]) <= 0) {
+			$errors->add('validation', __('Please enter an email address for Course Registration.'));
+		}
+		if (strlen($_POST["naiop_phone"]) <= 0) {
+			$errors->add('validation', __('Please enter a phone number for Course Registration.'));
+		}
+	}
+	//error_log(print_r($data, true));
+	//error_log(print_r($_POST, true));
 }
